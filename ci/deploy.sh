654c3ff55497d15b05a7254441661627ec423ce1
Marvin Roger <nivramdu94@gmail.com>
2016-02-26 14:14:42 +0100

    Trigger build

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,32 +1,32 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 echo "Deploy script"
 
-if [ $TRAVIS_NODE_VERSION != "stable" ]
+if [ "$TRAVIS_NODE_VERSION" != "stable" ]
 then
   echo "Job is not using stable node, exiting..."
   exit 0
 fi
 
-if [ $TRAVIS_PULL_REQUEST != "false" ]
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
   echo "Pull request, exiting..."
   exit 0
 fi
 
-if [ $TRAVIS_BRANCH != "master" ]
+if [ "$TRAVIS_BRANCH" != "master" ]
 then
   echo "Branch is not master, exiting..."
   exit 0
 fi
 
-echo $TRAVIS_PULL_REQUEST
-echo $TRAVIS_BRANCH
-echo $NPM_TOKEN
-echo $APPVEYOR_TOKEN
-echo $TRAVIS_COMMIT
-echo $TRAVIS_NODE_VERSION
-echo $TRAVIS_TAG
+echo "$TRAVIS_PULL_REQUEST"
+echo "$TRAVIS_BRANCH"
+echo "$NPM_TOKEN"
+echo "$APPVEYOR_TOKEN"
+echo "$TRAVIS_COMMIT"
+echo "$TRAVIS_NODE_VERSION"
+echo "$TRAVIS_TAG"
 
 if [ ! -v TRAVIS_TAG ]
 then
