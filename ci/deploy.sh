53199f43d814f7752063a62f277d16a4de78a1c1
Marvin Roger <nivramdu94@gmail.com>
2016-02-26 13:09:29 +0100

    Try to debug deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Adding .npmrc"
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
+echo "Publishing to npm"
 cd dist/
 npm publish
+echo "Triggering Appveyor build"
 curl -H "Content-type: application/json" -H "Authorization: Bearer $APPVEYOR_TOKEN" -X POST -d "{ accountName: \"marvinroger\", projectSlug: \"homie-server\", branch: \"master\", commitId: \"$TRAVIS_COMMIT\" }" https://ci.appveyor.com/api/builds
