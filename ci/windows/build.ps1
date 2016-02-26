e9757adfb82f2167d0cad13ddf33ecea8ad7123b
Marvin Roger <nivramdu94@gmail.com>
2016-02-26 02:38:04 +0100

    Add CI to build a Windows installer

---
+++ ci/windows/build.ps1
@@ -0,0 +1,9 @@
+$package = (Get-Content "$PSScriptRoot\..\..\package.json" -Raw) | ConvertFrom-Json
+$packageCompatibleNode = $package.engines.node
+$latestCompatibleNode = (Invoke-WebRequest "https://semver.io/node/resolve/$packageCompatibleNode").Content
+$currentArchitecture = "x86" # If ([Environment]::Is64BitProcess) { "x64" } else { "x86" }
+Invoke-WebRequest "https://nodejs.org/dist/v$latestCompatibleNode/win-$currentArchitecture/node.exe" -OutFile "$PSScriptRoot\sources\node.exe"
+npm install --prefix "$PSScriptRoot\sources" homie-server
+$packageVersion = $package.version
+$setupName = "homie-server-v$packageVersion-$currentArchitecture"
+iscc /dMySourceDir="$PSScriptRoot\sources" /dMyAppVersion="$packageVersion" /dMyAppOutput="$setupName" $PSScriptRoot\script.iss
