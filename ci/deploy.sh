7930725c32b7b08e4f80bcd7e4d7ece77d29ea0e
Marvin Roger <nivramdu94@gmail.com>
2016-02-26 14:35:01 +0100

    Trigger build

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -20,27 +20,18 @@ then
   exit 0
 fi
 
-echo "$TRAVIS_PULL_REQUEST"
-echo "$TRAVIS_BRANCH"
-echo "{$NPM_TOKEN}"
-echo "${$NPM_TOKEN}"
-echo "$\{$NPM_TOKEN}"
-echo "\${$NPM_TOKEN}"
-echo "$APPVEYOR_TOKEN"
-echo "$TRAVIS_COMMIT"
-echo "$TRAVIS_NODE_VERSION"
-echo "$TRAVIS_TAG"
-
-if [ ! -v TRAVIS_TAG ]
+if [ -z "$TRAVIS_TAG" ]
 then
   echo "Build is not tagged, exiting..."
   exit 0
 fi
 
-#echo "Adding .npmrc"
-#echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
-#echo "Publishing to npm"
-#cd dist/
-#npm publish
-#echo "Triggering Appveyor build"
-#curl -H "Content-type: application/json" -H "Authorization: Bearer $APPVEYOR_TOKEN" -X POST -d "{ accountName: \"marvinroger\", projectSlug: \"homie-server\", branch: \"master\", commitId: \"$TRAVIS_COMMIT\" }" https://ci.appveyor.com/api/builds
+echo "Adding .npmrc"
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
+
+echo "Publishing to npm"
+cd dist/ || exit
+npm publish
+
+echo "Triggering Appveyor build"
+curl -H "Content-type: application/json" -H "Authorization: Bearer $APPVEYOR_TOKEN" -X POST -d "{ accountName: \"marvinroger\", projectSlug: \"homie-server\", branch: \"master\", commitId: \"$TRAVIS_COMMIT\" }" https://ci.appveyor.com/api/builds
