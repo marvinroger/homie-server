0e53694c5b7a997c2bd91f1efd7aad49924f5797
Marvin Roger <nivramdu94@gmail.com>
2016-02-29 13:40:06 +0100

    Add automatic push of the windows build

--- ci/windows/build.ps1
+++ ci/windows/build.ps1
@@ -23,17 +23,45 @@ Copy-Item "$PSScriptRoot\sources\bundle\nssm-2.24\win32\nssm.exe" "$PSScriptRoot
 Remove-Item "$PSScriptRoot\sources\bundle\nssm-2.24" -Force -Recurse
 Remove-Item "$PSScriptRoot\sources\bundle\nssm.zip" -Force
 
+$packageVersion = $package.version
+
 # Install homie-server
-npm install --prefix "$PSScriptRoot\sources\bundle" homie-server
+npm install --prefix "$PSScriptRoot\sources\bundle" homie-server@$packageVersion
 Remove-Item "$PSScriptRoot\sources\bundle\etc" -Force -Recurse
 Remove-Item "$PSScriptRoot\sources\bundle\homie" -Force
 
 # Generate the installer
-$packageVersion = $package.version
 $setupName = "homie-server-v$packageVersion-$currentArchitecture"
 & "C:\Program Files (x86)\Inno Setup 5\iscc" /dMySourceDir="$PSScriptRoot\sources" /dMyAppVersion="$packageVersion" /dMyAppOutput="$setupName" $PSScriptRoot\script.iss
 
 # Push the installer to artifacts on Appveyor only
 if ($env:APPVEYOR -eq "True") {
   Push-AppveyorArtifact "$PSScriptRoot\output\$setupName.exe" -FileName "$setupName.exe" -DeploymentName "Homie Server v$packageVersion for Windows $currentArchitecture"
+
+  $releaseParams = @{
+    Uri = "https://api.github.com/repos/marvinroger/homie-server/releases/tags/v$packageVersion";
+    Method = 'GET';
+    Headers = @{
+      Authorization = 'Basic ' + [Convert]::ToBase64String(
+      [Text.Encoding]::ASCII.GetBytes($env:GITHUB_TOKEN + ":x-oauth-basic"));
+    }
+  }
+
+  $result = Invoke-RestMethod @releaseParams
+  $uploadUrl = $result | Select -ExpandProperty upload_url
+  $uploadUrl = $uploadUri -replace '\{\?name,label\}', "?name=$setupName.exe"
+  $uploadFile = Join-Path -path "$PSScriptRoot\output\" -childpath "$setupName.exe"
+
+  $uploadParams = @{
+    Uri = $uploadUrl;
+    Method = 'POST';
+    Headers = @{
+      Authorization = 'Basic ' + [Convert]::ToBase64String(
+      [Text.Encoding]::ASCII.GetBytes($env:GITHUB_TOKEN + ":x-oauth-basic"));
+    }
+    ContentType = 'application/x-msdownload';
+    InFile = $uploadFile
+  }
+
+  $result = Invoke-RestMethod @uploadParams
 }
